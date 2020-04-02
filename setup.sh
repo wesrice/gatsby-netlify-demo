b374882d3962bddfb5c0d327aee84b8994dc9932
Wes Rice <wrice@maxmind.com>
2020-04-02 00:02:48 -0400

    Add `volta` support

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ! -f "$HOME/.volta/bin/volta" ];
+then
+  curl https://get.volta.sh | bash
+fi
+
+$HOME/.volta/bin/volta install node@10
+$HOME/.volta/bin/volta pin node@10
+
+$HOME/.volta/bin/volta install yarn
+$HOME/.volta/bin/volta pin yarn
+
+if [[ "${CI}" == 'true' ]];
+then
+  $HOME/.volta/bin/yarn cms:install
+  $HOME/.volta/bin/yarn infrastructure:install
+fi
+
+exec $SHELL
